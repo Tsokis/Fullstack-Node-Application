e583ab26f17d8099d4dbb1da7bd922f70bc12382
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-05 01:12:22 +0200

    output response

--- app/public/js/ajaxCalls.js
+++ app/public/js/ajaxCalls.js
@@ -1,10 +1,12 @@
 
-   axios.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b81babb8580d7007d6bd828861eb6c8c')
-       .then(response => {
-           console.log(response);
+const appid='b81babb8580d7007d6bd828861eb6c8c';
+axios.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid='+appid)
+     .then(response => {
+         console.log(response);
+          output = document.querySelector('.output');
+          output.innerHTML = JSON.stringify(response.data);
        })
-       .catch(error => {
-           console.log(error);
+     .catch(error => {
+         console.log(error);
        });
-
-console.log(2+2);
+    
