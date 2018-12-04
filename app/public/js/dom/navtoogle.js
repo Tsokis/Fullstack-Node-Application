8471f4ba7751ff2c5ea14c8ad64220c39f22efc5
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-05 01:13:19 +0200

    navigation handler

--- app/public/js/dom/navtoogle.js
+++ app/public/js/dom/navtoogle.js
@@ -1,10 +1,11 @@
 
-document.querySelector('.bar').addEventListener('click', e =>{
-    e.preventDefault();
-    let nav = document.getElementById("nav");
-    if (nav.className === "main-nav") {
-        nav.className+= " responsive";
-    }else {
-        nav.className ="main-nav"
-    }
-});
+    document.querySelector('.bar').addEventListener('click', (e) => {
+        e.preventDefault();
+        let nav = document.getElementById("nav");
+        if (nav.className === "main-nav") {
+            nav.className += " responsive";
+        } else {
+            nav.className = "main-nav";
+        }
+    });
+
