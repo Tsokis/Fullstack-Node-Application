3b129850b2b6a1c9ab7914175e99844af0360e38
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-22 01:10:44 +0200

    rename

---
+++ app/public/js/ajax/weather.js
@@ -0,0 +1,40 @@
+const appKey = 'enter your api key';
+let searchButton = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city-name");
+let country = document.querySelector(".country");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temp");
+let humidity = document.getElementById("humidity-div");
+let customError = document.getElementById("error");
+
+searchButton.addEventListener("click", fetchWeather);
+//ajax request to target api
+function fetchWeather(){
+    if (searchInput.value === "") {
+        customError.style.display = 'block';
+        customError.innerHTML = "<p>You must enter something</p>"        
+    } else {
+        customError.style.display = 'none';
+        return fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchInput.value + '&units=metric'+
+            '&appid=' + appKey)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data)
+                let name = data.name
+                cityName.innerHTML = name;
+                icon.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+                temperature.innerHTML = data.main.temp + "°";
+                humidity.innerHTML = data.main.humidity + "%";
+                country.innerHTML = `Country ${data.sys.country}<br>
+                 max:${data.main.temp_max}<br>
+                 min:${data.main.temp_min}<br>
+                 description today:${data.weather[0].description}`;
+            })
+            .catch(error => console.log(error));
+    }
+ }
+   
+    
+
+    
