a628bfd1b4cabc7b546443e8f2130f1075a1e1b2
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-23 23:41:27 +0200

    Contact form model

---
+++ app/models/Contact.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ContactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
+
+const Contact = mongoose.model('Contact', ContactSchema);
+module.exports = Contact;
