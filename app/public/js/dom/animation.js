40aff3b3d8c231dbb26f879366af78c97699f148
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-23 21:16:02 +0200

    interactive animation

--- app/public/js/dom/animation.js
+++ app/public/js/dom/animation.js
@@ -1,7 +1,33 @@
 const canvas = document.getElementById('canvas')
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+
 const c = canvas.getContext('2d');
+// mouse object
+let mouse = {
+    x: undefined,
+    y: undefined
+};
+const maxRadius = 40;
+// const minRadius = 2;
+// color array 
+const colorArray = [
+    '#577170',
+    '#561C30',
+    '#92C9ED',
+    '#501D73',
+    '#008744',
+    '#4A6CA8'
+]
+// event listener
+window.addEventListener('mousemove',(e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+});
+// resize browser handling
+window.addEventListener('resize',()=>{
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init();
+});
 // circle object with draw and update methods
 function Circle(x,y,dx,dy,radius){
     this.x = x;
@@ -9,12 +35,14 @@ function Circle(x,y,dx,dy,radius){
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
     this.draw = () => {
          // make a circle
          c.beginPath();
          c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-         c.strokeStyle = 'blue';
-         c.stroke();
+         c.fillStyle = this.color;
          c.fill();
     }
     this.update = () => {
@@ -28,29 +56,40 @@ function Circle(x,y,dx,dy,radius){
          }
          this.x += this.dx;
          this.y += this.dy;
+         // interact with the circles
+         if(mouse.x - this.x < 50 && mouse.x - this.x > -50
+             && mouse.y -this.y < 50 && mouse.y - this.y > -50){
+                 if(this.radius <maxRadius){
+                     this.radius +=1;
+                 }
+         }else if (this.radius > this.minRadius){
+             this.radius -=1;
+         }
          // draw a circle
          this.draw();
-    }
-    
+    }    
 }
 //object cirlce instance
 //let circle = new Circle(200, 200, 3, 3, 30);
 //store the circles into an array
 let circleArray = [];
-// make new instances
-for (i=0; i< 100; i++){ 
-    let radius = 30;
-    let x = Math.random() * (innerWidth -radius *2) +radius;
-    let y = Math.random() * (innerHeight -radius *2) +radius;
-    //x velocity
-    let dx = (Math.random() - 0.5) * 10;
-    //y velocity
-    let dy = (Math.random() - 0.5) * 10;
-    
-    // push to circle array
-    circleArray.push(new Circle(x, y, dx, dy, radius));    
-}
 
+function init(){
+    circleArray = [];
+    // make new instances
+    for (i = 0; i < 800; i++) {
+        let radius = Math.random() * 3 + 1;
+        let x = Math.random() * (innerWidth - radius * 2) + radius;
+        let y = Math.random() * (innerHeight - radius * 2) + radius;
+        //x velocity
+        let dx = (Math.random() - 0.5) * 7;
+        //y velocity
+        let dy = (Math.random() - 0.5) * 7;
+
+        // push to circle array
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+    }
+}
 //animation function
 function animate(){
     requestAnimationFrame(animate);
@@ -60,5 +99,5 @@ function animate(){
         circleArray[i].update();
     } 
 }
-
+init();
 animate();
