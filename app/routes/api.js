8411548c22bed6644585d1a4d74fc28d6be4581c
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-23 18:47:00 +0200

    added async/await and redirecr

--- app/routes/api.js
+++ app/routes/api.js
@@ -8,15 +8,19 @@ apiController.use(bodyParser.urlencoded({
     extended: true
 }));
 
-apiController.get('/api', (req, res) => {
-     Comments.find({}, (err, Comments) => {
+apiController.get('/api', async (req, res) => {
+     let comments = await Comments.find({}, (err, Comments) => {
      res.json(Comments);
      });
+     return comments;
 });
 
-apiController.post('/api', (req, res) => {
-            let comment = new Comments(req.body);
-            comment.save();
-            res.status(201).send(comment);        
+apiController.post('/api',async (req, res) => {
+            let comment = await new Comments(req.body);
+            comment.save();                       
+            // posting the json
+            //res.status(201).send(comment);   
+            //redirect to test.ejs page after commit to the database
+            res.redirect('test');     
 });
 module.exports = apiController;
