8ff6d981b06ff837476e7e4fee82a2410b426037
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-23 18:42:09 +0200

    async fetch from database and post from the user form to database

--- app/public/js/ajax/test-ajax.js
+++ app/public/js/ajax/test-ajax.js
@@ -1,15 +1,43 @@
 //Fetch data from models/Comments
 function getData() {
     fetch('http://localhost:3002/api')
-    .then(response => response.json())
-    .then(data => {
-        //select css class .test [for the output]
-        let test = document.querySelector('.test');
-        data.forEach(value => {            
-            test.innerHTML += `<li>Comment Title: ${value.title} and Comment Body: ${value.body}</li>`;
-        });
-    })
-    .catch(err => console.log(err));
+        .then(response => response.json())
+        .then(data => {
+            //select css class .test [for the output]
+            let test = document.querySelector('.test');
+            data.forEach(value => {
+                test.innerHTML += `<li>Comment Title: ${value.title} and Comment Body: ${value.body}</li>`;
+            });
+        })
+        .catch(error => console.log(error));
 }
+
+// client post data and append to ul on button click
+let button = document.getElementById('button').addEventListener('click', e =>{
+    e.preventDefault();
+    let title = document.getElementById("title");
+    let body = document.getElementById("body");     
+    let ul = document.getElementById("test");
+    let li = document.createElement("li");    
+    if (title.value && body.value===null){
+        document.body.innerHTML = "<h1>Fuck Cunt</h1>";
+    }else {
+        axios.post('http://localhost:3002/api', {
+                title: title.value,
+                body: body.value
+        })
+        .then(response => {
+            let dataSend = JSON.parse(response.config.data);
+            let title = dataSend.title;
+            let body = dataSend.body;    
+            li.innerHTML = `Title:${title} and Message:${body}`;
+            ul.appendChild(li);                       
+            console.log(dataSend);       
+        })
+        .catch(error => {
+           console.log(error);
+        });
+    }        
+});
 
 getData();
