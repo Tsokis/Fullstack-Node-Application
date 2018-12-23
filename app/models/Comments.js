1b523e09f3f36ad8a1ee225f6ff32fec570f61e8
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-24 01:42:20 +0200

    added name field to database

--- app/models/Comments.js
+++ app/models/Comments.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const CommentsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
     title: {
         type: String,
         required: true,
