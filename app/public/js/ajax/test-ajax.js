ceb11023d826bd90c2c3912988f0ed5498c33732
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-24 01:43:12 +0200

    added simple client side form validation

--- app/public/js/ajax/test-ajax.js
+++ app/public/js/ajax/test-ajax.js
@@ -1,43 +1,50 @@
 //Fetch data from models/Comments
 function getData() {
     fetch('http://localhost:3002/api')
-        .then(response => response.json())
-        .then(data => {
-            //select css class .test [for the output]
-            let test = document.querySelector('.test');
-            data.forEach(value => {
-                test.innerHTML += `<li>Comment Title: ${value.title} and Comment Body: ${value.body}</li>`;
-            });
-        })
-        .catch(error => console.log(error));
+    .then(response => response.json())
+    .then(data => {
+        //select css class .test [for the output]
+        let test = document.querySelector('.test');
+        data.forEach(value => {
+            test.innerHTML += `<li>${value.title} <strong>${value.name}</strong> said: ${value.body}</li>`;
+        });
+    })
+    .catch(error => console.log(error));
 }
 
-// client post data and append li to ul on button click
+// client post data and append li to ul on button click and reset fields
 let button = document.getElementById('button').addEventListener('click', e =>{
-    e.preventDefault();
+    e.preventDefault();    
+    let name = document.getElementById("name")
     let title = document.getElementById("title");
     let body = document.getElementById("body");     
     let ul = document.getElementById("test");
+    let error = document.getElementById("error");
+    error.style.display = 'none';
     let li = document.createElement("li");    
-    if (title.value && body.value===null){
-        document.body.innerHTML = "<h1>F C</h1>";
-    }else {
-        axios.post('http://localhost:3002/api', {
-                title: title.value,
-                body: body.value
-        })
-        .then(response => {
-            let dataSend = JSON.parse(response.config.data);
-            let title = dataSend.title;
-            let body = dataSend.body;    
-            li.innerHTML = `Title:${title} and Message:${body}`;
-            ul.appendChild(li);                       
-            console.log(dataSend);       
-        })
-        .catch(error => {
-           console.log(error);
-        });
-    }        
+    if (name.value || title.value || body.value ===""){
+        error.style.display = 'block';
+        error.innerHTML = `<strong>you must enter the fields</strong>`;
+    } else{
+    axios.post('http://localhost:3002/api', {
+        title: title.value,
+        body: body.value,
+        name: name.value
+     })
+    .then(response => {
+        let dataSend = JSON.parse(response.config.data);
+        let name = dataSend.name
+        let title = dataSend.title;
+        let body = dataSend.body;    
+        li.innerHTML = `For ${title} subject  <strong>${name}</strong> said: ${body}`;
+        ul.appendChild(li); 
+        console.log(dataSend);
+        document.commentForm.reset();            
+    })
+    .catch(error => {
+       console.log(error);
+    });            
+    }   
 });
 
 getData();
