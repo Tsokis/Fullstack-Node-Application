fb00bbb12e0de760a9dc6de70cd29b008639773f
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-25 00:30:05 +0200

    fixed client bug validation

--- app/public/js/ajax/test-ajax.js
+++ app/public/js/ajax/test-ajax.js
@@ -19,32 +19,33 @@ let button = document.getElementById('button').addEventListener('click', e =>{
     let title = document.getElementById("title");
     let body = document.getElementById("body");     
     let ul = document.getElementById("test");
+    let li = document.createElement("li");  
     let error = document.getElementById("error");
-    error.style.display = 'none';
-    let li = document.createElement("li");    
-    if (name.value || title.value || body.value ===""){
-        error.style.display = 'block';
+    error.style.display = 'none';      
+    if (name.value || title.value || body.value !==""){
+        axios.post('http://localhost:3002/api', {
+            title: title.value,
+            body: body.value,
+            name: name.value
+        })
+        .then(response => {
+            let dataSend = JSON.parse(response.config.data);
+            let name = dataSend.name
+            let title = dataSend.title;
+            let body = dataSend.body;
+            li.innerHTML = `For ${title} subject  <strong>${name}</strong> said: ${body}`;
+            ul.appendChild(li);
+            console.log(dataSend);
+            document.commentForm.reset();
+        })
+        .catch(error => {
+            console.log(error);
+        });         
+        
+    }else {
+        error.style.display = "block";
         error.innerHTML = `<strong>you must enter the fields</strong>`;
-    } else{
-    axios.post('http://localhost:3002/api', {
-        title: title.value,
-        body: body.value,
-        name: name.value
-     })
-    .then(response => {
-        let dataSend = JSON.parse(response.config.data);
-        let name = dataSend.name
-        let title = dataSend.title;
-        let body = dataSend.body;    
-        li.innerHTML = `For ${title} subject  <strong>${name}</strong> said: ${body}`;
-        ul.appendChild(li); 
-        console.log(dataSend);
-        document.commentForm.reset();            
-    })
-    .catch(error => {
-       console.log(error);
-    });            
-    }   
+    } 
 });
 
 getData();
