b497244699c88dcd9098f49c30adf3b6048bf2be
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-31 23:10:32 +0200

    comments

--- app/public/js/ajax/tvshows.js
+++ app/public/js/ajax/tvshows.js
@@ -7,15 +7,15 @@ searchButton.addEventListener("click",() => {
             console.log(res.data);
             let responseArray = res.data;
             let output = document.querySelector('.output');
-            // let icon = document.getElementById("icon");
+            //let icon = document.getElementById("icon");
             //test
             responseArray.forEach((item) => {
                 // let image = item.show.image.medium;
                 // console.log(image);
-                output.innerHTML += `<li>${item.show.name} with rating ${item.show.rating.average}</li><br>
+                output.innerHTML += `<li class="card">${item.show.name} with rating ${item.show.rating.average}</li><br>
                 <li>${item.show.genres} and currently ${item.show.status}</li>`;
-                //image output works but if null from the api doesn't display
-                // icon.src += `${image}`;
+                //image output works but if its null from the api the search output doesn't display property
+                //icon.src += `${image}`;
             });            
         })
         .catch((error) => {
